e5fca21e404bc8a8f9d63702dc0fb179db67265e
Jace <jacepp@hotmail.com>
2013-09-12 09:18:31 -0700

    getting there

--- app/js/controllers/ProfileController.js
+++ app/js/controllers/ProfileController.js
@@ -3,27 +3,7 @@
 foodMeApp.controller('ProfileController',
     function ProfileController($scope, customer) {
 
-  $scope.editedItem = null;
-  $scope.cName = { name: customer.name, editing: false };
-  $scope.cAddress = { name: customer.address, editing: false };
-
-  $scope.startEditing = function(item){
-    item.editing=true;
-    $scope.editedItem = item;
-  }
-
-  $scope.doneEditing = function(key, item){
-    switch (key) {
-    case 'name':
-      customer.name = item.name;
-        break;
-      case 'address':
-        customer.address = item.name;
-        break;
-    }
-    item.editing=false;
-    $scope.editedItem = null;
-  }
+  $scope.favorites = [];
 
   $scope.filterFavorites = function() {
     var array = [];
@@ -41,3 +21,27 @@ foodMeApp.controller('ProfileController',
   };
 
 });
+
+function InfoController($scope, customer) {
+  $scope.editedItem = null;
+  $scope.cName = { name: customer.name, editing: false };
+  $scope.cAddress = { name: customer.address, editing: false };
+
+  $scope.startEditing = function(item){
+    item.editing=true;
+    $scope.editedItem = item;
+  }
+
+  $scope.doneEditing = function(key, item){
+    switch (key) {
+    case 'name':
+      customer.name = item.name;
+      break;
+    case 'address':
+      customer.address = item.name;
+      break;
+    }
+    item.editing=false;
+    $scope.editedItem = null;
+  }
+}
